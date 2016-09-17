355c2dff76ef8cb5963c6bcc787909d94d9b3e3a
Sathyanarayan09 <sathyanarayan065@gmail.com>
2016-09-17 19:54:40 +0530

    Add files via upload

---
+++ ui/Index.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html>
+<head>
+<title>Home</title>
+    
+    	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="stylesheet" href="css/themes/default/jquery.mobile-1.4.5.min.css" />
+	<script src="js/jquery.min.js"></script>
+	<script src="js/jquery.mobile-1.4.5.min.js"></script>
+    <style type="text/css">
+       @media all {
+.bg { background-color:;
+background-image:url("logo1.jpg");
+background-position:center center;
+background-repeat:no-repeat;
+background-attachment:fixed;
+background-size:cover;
+ }
+} 
+      
+</style>
+</head>
+<body >
+    
+    
+  <div data-role="page" style="background-image: url('logo1.jpg'); background-attachment: fixed; background-repeat: no-repeat; background-size: 100% 100%;">
+	
+<div data-role="header" data-position="fixed" data-theme="b">
+		<h1 align="center">Welcome</h1>
+    
+   <a href="#mypanel" class="ui-btn ui-icon-bars ui-btn-icon-notext ui-corner-all" data-theme="a"> LIST</a>
+    <div data-role="panel" id="mypanel" data-position-fixed="true" data-display="overlay" data-swipe-close="ture" data-theme="b">
+</div></div>  
+    
+    <div class="ui-grid-b ui-responsive">
+    <div class="ui-block-a" class="ui-btn ui-shadow ui-corner-all ui-btn-icon-right ui-icon-arrow-r">
+        
+        
+        <a href="login.php" class="ui-btn ui-shadow ui-corner-all ui-btn-icon-right ui-icon-arrow-r" value="Login" name="submit" >Login</a></div>
+    
+</div>
+
+
+
+
+   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+     </div>  
+    
+    </body>
+    
