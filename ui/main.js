38e6c2713409b5175cc2392d664df414493ca643
Sathyanarayan09 <sathyanarayan065@gmail.com>
2016-09-23 22:39:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,12 +3,12 @@
 var button = document.getElementById('counter');
 var counter = 0;
 
-button.onClick =function() {
-    
-    counter = counter + 1;
+
+
+window.onload = function () {
+    document.getElementById('counter').onclick=change;
+      counter = counter + 1;
     var span= document.getElementById('count');
   span.innerHTML = counter.toString();
-    
-
     
 };
