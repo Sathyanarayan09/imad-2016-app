315266fd9d7acec3a9f5e4dd5a04a87e90affd45
Sathyanarayan09 <sathyanarayan065@gmail.com>
2016-09-23 21:46:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 var c=0;
-app.get('/coumment', function(req, res){
+app.get('/comment', function(req, res){
     
   c=c+1;  
   res.send(c.toString());
