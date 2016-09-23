3362d76244c6d87c8f0fec11aa3a988e5f6bc357
Sathyanarayan09 <sathyanarayan065@gmail.com>
2016-09-23 22:16:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 var button = document.getElementById('c');
 var c=0;
 
-button.onClick = function(){
+button.onClick = function() {
     
     c = c + 1;
     var span= document.getElementById('count');
