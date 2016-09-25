d33076498530671c0c78dd9687130c738eb83c20
Sathyanarayan09 <sathyanarayan065@gmail.com>
2016-09-25 11:10:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,25 @@ var button = document.getElementById('counter');
 var counter = 0;
 
 button.onclick =function() {
+    //creat a request object
+    var request= new XMLHTTPRequest();
+    
+    //capture the responce and store it in a variable
+    
+    reques.onreadystatchange = function(){
+      if ( request.readyState === XMLHTTPRequest.Done)
+      {
+          
+          if(request.State === 200)
+          {
+              var counter = request.responseText;
+              var span = document.getElementById('count');
+              span.innerHTML = counter.toString();
+              
+          }
+      }
+        
+    };
     
     counter = counter + 1;
     var span= document.getElementById('count');
