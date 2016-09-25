51dc9d8ddd00a1aae1826d36dd93ebf4c5ff711b
Sathyanarayan09 <sathyanarayan065@gmail.com>
2016-09-25 10:40:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,12 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-
+var c=0;
+app.get('/comment', function(req, res){
+    
+  c=c+1;  
+  res.send(c.toString());
+});
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -25,17 +30,10 @@ app.get('/artical-two', function(req, res)
 {
   res.sendFile(path.join(__dirname, 'ui', 'artical-two.html'));
 });
-
-
-
-
-
-
-
-
-
-
-
+app.get('/artical-three', function(req, res)
+{
+     res.sendFile(path.join(__dirname, 'ui', 'artical-three.html'));
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
