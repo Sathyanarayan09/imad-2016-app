0c0fbfc8912ba91c5514247d5ea0bf887eba3147
Sathyanarayan09 <sathyanarayan065@gmail.com>
2016-09-25 15:36:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,28 @@
+var button = document.getElementById('counter');
 
-var button= document.getElementById('counter');
-var counter = 0;
-
-
-button.onclick = function(){
+button.onclick =function() {
+    //creat a request object
+    var request= new XMLHttpRequest();
     
-   counter=counter+1;
-    var span= document.getElementById('count');
-  span.innerHTML = counter.toString();
+    //capture the responce and store it in a variable
+    
+    request.onreadystatchange = function(){
+      if ( request.readyState === XMLHttpRequest.DONE)
+      {
+          
+          if(request.status === 200)
+          {
+              var counter = request.responseText;
+              var span = document.getElementById('count');
+              span.innerHTML = counter.toString();
+              
+          }
+      }
+        
+    };
+    //make the request
+   request.open('GET','http://sathyanarayan09.imad.hasura-app.io/counter', true);
+   reques.send(null);
+
     
 };
