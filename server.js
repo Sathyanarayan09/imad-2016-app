3598d81b280d769aab653d44062f8913b132c09d
Sathyanarayan09 <sathyanarayan065@gmail.com>
2016-09-25 11:40:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,46 +1,17 @@
-var express = require('express');
-var morgan = require('morgan');
-var path = require('path');
-
-var app = express();
-app.use(morgan('combined'));
-
-var counter=0;
-app.get('/counter', function(req, res){
-    
-  counter=counter+1;  
-  res.send(counter.toString());
-});
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-
-app.get('/artical-one', function(req, res)
-{
-   res.sendFile(path.join(__dirname, 'ui', 'artical-one.html'));
-});
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
+<!DOCTYPE html>
+<html lang="en">
+<title> Artial-one </title>
 
 
+<body>
 
-app.get('/artical-two', function(req, res)
-{
-  res.sendFile(path.join(__dirname, 'ui', 'artical-two.html'));
-});
+    <a href='/'> Back </a>
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-app.get('/ui/j.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'j.jpg'));
-});
+This <button id="counter">click me!  </button> This button has clicked <span id="count"> 0 </span>times.
 
 
-var port = 8080; // Use 8080 for local development because you might already have apache running on 80
-app.listen(8080, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
-});
+        <script type="text/javascript" src="/ui/main.js"> </script>
+    </body>
+
+
+</html>
