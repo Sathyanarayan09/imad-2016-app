daee2e8c450dd828b64aacdf33379d7bb62025d5
Sathyanarayan09 <sathyanarayan065@gmail.com>
2016-10-29 12:00:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,28 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-
+var counter=0;
+app.get('/counter', function(req, res){
+    
+  counter=counter+1;  
+  res.send(counter.toString());
+});
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+
+var names = [];
+app.get('/submit-name', function (req, res)
+{ //get the name from request
+  var name = req.query.name;
+  
+  names.push(name);
+  //JSON
+  
+  res.send(JSON.stringify(names));
+});
 
 
 
