970667b4712bc4de2516d665f9ece4c08b79a7d3
Sathyanarayan09 <sathyanarayan065@gmail.com>
2016-11-01 00:39:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.get('/', function (req, res) {
 var pool = new Pool(config);
 app.get('/test-db', function (req, res) {
 
- pool.query('SELECT * FROM articals', function(err, result) {
+ pool.query('SELECT * FROM articals', function(err, result.rows) {
      
      
      
