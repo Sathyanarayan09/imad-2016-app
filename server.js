68afa4e2f03ce7a820aca2c94a6787ebfa53af3c
Sathyanarayan09 <sathyanarayan065@gmail.com>
2016-10-31 10:13:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -26,7 +26,9 @@ app.get('/ui/blog.html', function(req, res)
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
 app.get('/ui/image.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'image.jpg'));
 });
