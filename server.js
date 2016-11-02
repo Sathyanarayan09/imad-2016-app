526b4fe58e7bde4a052aab328d79e2232f9066b0
Sathyanarayan09 <sathyanarayan065@gmail.com>
2016-11-03 00:04:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ pool.query("SELECT * FROM articals where title = '"+ req.params.articalname+"'",
         }
         else{
             
-            var articalDate = result.rown[0];
+            var articalDate = result.rows[0];
             res.send(createTemplate(articalDate))
         }
      }
