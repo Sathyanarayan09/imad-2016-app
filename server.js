e05e5340b393d3e1f7b0ff8ae302e897c177be93
Sathyanarayan09 <sathyanarayan065@gmail.com>
2016-11-03 18:37:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,7 +22,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-
+app.get('/login.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'login.html'));
+});
 
 
 var pool = new Pool(config);
