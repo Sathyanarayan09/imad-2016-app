c763bab4bd77079f2992c6de52d7af76039d3cfd
Sathyanarayan09 <sathyanarayan065@gmail.com>
2016-11-18 18:43:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,18 +6,9 @@ var app = express();
 app.use(morgan('combined'));
 
 var Pool = require('pg').Pool;
-var config = {
-    user: 'sathyanarayan09',
-    database: 'sathyanarayan09',
-    host: 'db.imad.hasura-app.io',
-    port: '5432',
-  
-  password: process.env.DB_PASSWORD
- 
-  
-};
 
-var crypto = reques('crypto');
+
+
 
 
 app.get('/', function (req, res) {
@@ -28,66 +19,6 @@ app.get('/ui/login.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'login.html'));
 });
 
-
-var pool = new Pool(config);
-app.get('/test-db', function (req, res) {
-
- pool.query('SELECT * FROM articals', function(err, result) {
-     
-     
-     
-     if(err)
-     {
-         
-         res.status(500).send(err.toString());
-     }
-     
-     else
-     {
-         
-         res.send(JSON.stringify(result.rows));
-     }
-}); 
-
-});
-
-app.get('/articals/:articalname', function(req, res)
-{
- 
- 
- 
-pool.query("SELECT * FROM articals where title = '"+ req.params.articalname+"'",function(err, result) {
-      
-     
-     
-     
-     if(err)
-     {
-         
-         res.status(500).send(err.toString());
-     }
-     
-     else
-     {
-         
-        if(result.rows.length===0)
-        {
-            res.status(404).send('Artical not found');
-            
-        }
-        else{
-            
-            var articalDate = result.rows[0];
-            res.send(createTemplate(articalDate))
-        }
-     }
-}); 
- 
- 
- 
- 
- 
-});
 
 
 
@@ -103,9 +34,13 @@ app.get('/ui/blog.html', function(req, res)
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+
+
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
+
+
 app.get('/ui/image.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'image.jpg'));
 });
@@ -132,16 +67,12 @@ app.get('/ui/coff.jpg', function (req, res) {
 app.get('/ui/j.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'j.jpg'));
 });
-app.get('/ui/four.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'four.jpg'));
-});
+
+
 app.get('/ui/three.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'three.jpg'));
 });
 
-app.get('/ui/bg.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'bg.jpg'));
-});
 
 
 app.get('/ui/two.jpg', function (req, res) {
@@ -156,26 +87,10 @@ app.get('/ui/th.jpg', function (req, res) {
 
 
 
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
-request.open('GET','http://sathyanarayan09.imad.hasura-app.io/submit-name?name='+name,true);
-reques.send(null);
-
 
 /*login codes*/
 
-function hash(input, salt){
-    var hashed = crypto.pbkdf2Sync('secret', 'salt', 100000, 512, 'sha512');
-    return hashed.toString('hex');
-}
 
-
-app.get('hash/login', function (req, res) {
-
-var hashedString = has(req.params.input, 'thisisrandomstring');
-res.send(hashedString);
-
-});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
