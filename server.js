d7616986666a61f186c966c7ab3beca3fb99358a
Sathyanarayan09 <sathyanarayan065@gmail.com>
2016-11-20 18:04:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -84,7 +84,7 @@ app.get('/ui/three.jpg', function (req, res) {
 });
 
 app.get('/ui/four.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'three.jpg'));
+  res.sendFile(path.join(__dirname, 'ui', 'four.jpg'));
 });
 
 
