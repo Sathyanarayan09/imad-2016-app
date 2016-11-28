5e113fc5dca2737b0f861bd1cbee5035cfb428f9
Sathyanarayan09 <sathyanarayan065@gmail.com>
2016-11-28 20:11:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,9 +6,25 @@ var app = express();
 app.use(morgan('combined'));
 
 var Pool = require('pg').Pool;
+var crypto = require('crypto');
+var bodyParser = require('body-parser');
+var session = require('express-session');
 
+var config={
+    user : 'sathyanarayan09',
+    database :'sathyanarayan09',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password:process.env.DB_PASSWORD
+};
 
-
+var app = express();
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(session({
+    secret: 'someRandomSecretValue',
+    cookie: { maxAge: 1000 * 60 * 60 * 24 * 30}
+}));
 
 
 app.get('/', function (req, res) {
@@ -146,6 +162,52 @@ app.get('/ui/img/th.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui/img', 'th.jpg'));
 });
 /*login codes*/
+
+
+
+function hash (input, salt) {
+    // How do we create a hash?
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    return ["pbkdf2", "10000", salt, hashed.toString('hex')].join('$');
+}
+
+
+app.get('/hash/:input', function(req, res) {
+   var hashedString = hash(req.params.input, 'this-is-some-random-string');
+   res.send(hashedString);
+});
+
+app.post('/create-user', function (req, res) {
+   // username, password
+   // {"username": "tanmai", "password": "password"}
+   // JSON
+   var username = req.body.username;
+   var password = req.body.password;
+   var salt = crypto.randomBytes(128).toString('hex');
+   var dbString = hash(password, salt);
+   pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function (err, result) {
+      if (err) {
+          res.status(500).send(err.toString());
+      } else {
+          res.send('User successfully created: ' + username);
+      }
+   });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
