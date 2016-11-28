ae091cbfbd9496fed1c9acd5807d0bd979e72b26
Sathyanarayan09 <sathyanarayan065@gmail.com>
2016-11-28 21:29:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
@@ -6,14 +7,14 @@ var crypto = require('crypto');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 
-
-var config={
-    user : 'sathyanarayan09',
-    database :'sathyanarayan09',
-    host:'db.imad.hasura-app.io',
-    port:'5432',
-    password:process.env.DB_PASSWORD
+var config = {
+    user: 'sathyanarayan09',
+    database: 'sathyanarayan09',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
 };
+
 
 var app = express();
 app.use(morgan('combined'));
@@ -22,7 +23,6 @@ app.use(session({
     secret: 'someRandomSecretValue',
     cookie: { maxAge: 1000 * 60 * 60 * 24 * 30}
 }));
-
 
 
 app.get('/', function (req, res) {
@@ -161,6 +161,9 @@ app.get('/ui/img/th.jpg', function (req, res) {
 });
 /*login codes*/
 
+
+
+
 function hash (input, salt) {
     // How do we create a hash?
     var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
@@ -186,42 +189,6 @@ app.post('/create-user', function (req, res) {
           res.status(500).send(err.toString());
       } else {
           res.send('User successfully created: ' + username);
-      }
-   });
-});
-
-
-
-
-app.post('/loginn', function (req, res) {
-   var username = req.body.username;
-   var password = req.body.password;
-   
-   pool.query('SELECT * FROM "user" WHERE username = $1', [username], function (err, result) {
-      if (err) {
-          res.status(500).send(err.toString());
-      } else {
-          if (result.rows.length === 0) {
-              res.status(403).send('username/password is invalid');
-          } else {
-              // Match the password
-              var dbString = result.rows[0].password;
-              var salt = dbString.split('$')[2];
-              var hashedPassword = hash(password, salt); // Creating a hash based on the password submitted and the original salt
-              if (hashedPassword === dbString) {
-                
-                // Set the session
-                req.session.auth = {userId: result.rows[0].id};
-                // set cookie with a session id
-                // internally, on the server side, it maps the session id to an object
-                // { auth: {userId }}
-                
-                res.send('credentials correct!');
-                
-              } else {
-                res.status(403).send('username/password is invalid');
-              }
-          }
       }
    });
 });
