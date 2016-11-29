8f5a6deadf4c6ce7955464595d98abe209dad9b8
Sathyanarayan09 <sathyanarayan065@gmail.com>
2016-11-29 13:37:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -247,7 +247,7 @@ app.get('/logout', function (req, res) {
         <div class="jumbotron" style="box-shadow: 2px 2px 4px #000000;">
             <h2 class="text-center">Thank you for signing up for <span style="color:#F89406;">The</span><span style="color:#26A65B;">Winning</span><span style="color:#19B5FE;">Number</span></h2>
             <center><div class="btn-group" style="margin-top:50px;">
-                <a href="#" class="btn btn-lg btn-primary">Home</a>
+                <a href="/" class="btn btn-lg btn-primary">Home</a>
                 <a href="#" class="btn btn-lg btn-default">Login</a>
             </div></center>
         </div>
