dafaf29889bda6f26b54da9ea377baa1bdf9f114
Sathyanarayan09 <sathyanarayan065@gmail.com>
2016-11-29 14:41:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -236,7 +236,7 @@ app.get('/check-login', function (req, res) {
            if (err) {
               res.status(500).send(err.toString());
            } else {
-              res.send(result.rows[0].username);    
+               
               res.redirect('/ui/postarticals.html');
            }
        });
