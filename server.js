c346f4aab9a2ee5a70d6bb760e776ba715d6386b
Sathyanarayan09 <sathyanarayan065@gmail.com>
2016-11-29 13:56:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -242,7 +242,7 @@ app.get('/check-login', function (req, res) {
 
 app.get('/logout', function (req, res) {
    delete req.session.auth;
-   res.send('<html><head><link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> </head><body><div class="container" style="margin-top:5%;"><div class="row"><div class="jumbotron" style="box-shadow: 2px 2px 4px #000000;"><h2 class="text-center">Thank you for signing up for <span style="color:#F89406;">The</span><span style="color:#26A65B;">Winning</span><span style="color:#19B5FE;">Number</span></h2><center><div class="btn-group" style="margin-top:50px;"> <a href="/" class="btn btn-lg btn-primary">Home</a><a href="/ui/login.html" class="btn btn-lg btn-default">Login</a></di></center></div></div></div></html>');
+   res.send('<html><head><link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> </head><body><div class="container" style="margin-top:5%;"><div class="row"><div class="jumbotron" style="box-shadow: 2px 2px 4px #000000;"><h2 class="text-center">Thank you for signing up <span style="color:#F89406;">Stay</span><span style="color:#26A65B;">In</span><span style="color:#19B5FE;">Touch!</span></h2><center><div class="btn-group" style="margin-top:50px;"> <a href="/" class="btn btn-lg btn-primary">Home</a><a href="/ui/login.html" class="btn btn-lg btn-default">Login</a></di></center></div></div></div></html>');
 });
 
 var pool = new Pool(config);
