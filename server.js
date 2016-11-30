e664c5275019fd6f605ca0488b85e6a9c82b7db9
Sathyanarayan09 <sathyanarayan065@gmail.com>
2016-11-30 22:09:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ function createTemplate (data) {
               ${title}
           </title>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link href="/ui/style.css" rel="stylesheet" />
+      
       </head> 
       <body>
           <div class="container">
@@ -54,7 +54,7 @@ function createTemplate (data) {
                 ${content}
               </div>
               <hr/>
-              <p>Comments</p>
+              <h4>Comments</h4>
               <div id="comment_form">
               </div>
               <div id="comments">
