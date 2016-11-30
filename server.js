4875ac8dd158204ec7769dd3fdf780eda13f3b73
Sathyanarayan09 <sathyanarayan065@gmail.com>
2016-11-30 11:57:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -296,7 +296,7 @@ var pool = new Pool(config);
 app.get('/get-articles', function (req, res) {
    // make a select request
    // return a response with the results
-   pool.query('SELECT * FROM articles ORDER BY date DESC', function (err, result) {
+   pool.query('SELECT * FROM articles ORDER BY title DESC', function (err, result) {
       if (err) {
           res.status(500).send(err.toString());
       } else {
