f23eeea000be49d6522124b43bc41f2c8e1c2d94
Sathyanarayan09 <sathyanarayan065@gmail.com>
2016-11-30 21:20:14 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -1,48 +1,51 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Articles</title>
- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    
-	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-   <meta name="viewport" content="width=device-width, initial-scale=1">
-	       <meta http-equiv="X-UA-Compatible" content="IE=edge">
-		     <meta charset="utf-8">
-		       <link rel="icon" type="/ui/ico.png" href="/ui/ico.png"/>
-		     </head>
-<body >
-    <div class="container">
-          <div class="row">
-              <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-                <div id="article">
-                <center>Loading Articles...</center>
+function loadArticles () {
+       var acticle = `
+  <h3>Login/Register Here for Posting Comments on the Articles!</h3>
+        <div class="row control-group">
+            <div class="form-group col-xs-12 floating-label-form-group controls">
+              <label>Username</label>
+                <input type="text" class="form-control" id="username" placeholder="Enter Your Username" required>
             </div>
-            <hr/>
-          
-        
-
         </div>
-    </div>
-    </div>
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    <footer>
-            <div class="row">
-                <div class="col-lg-12">
-                  <p align="center">Sathyanarayan | 2016</p>
-                </div>
+        <div class="row control-group">
+            <div class="form-group col-xs-12 floating-label-form-group controls">
+              <label>Password</label>
+                <input type="password" class="form-control" id="password" placeholder="Password" required>
             </div>
-            <!-- /.row -->
-        </footer>
-         <script type="text/javascript" src="/ui/article.js">
-</script>
-</body>
-</html>
+        </div>
+        <br/>
+        <input type="submit" class="btn btn-default" id="login_btn" value="Login" />
+        <input type="submit" class="btn btn-default" id="register_btn" value="Register" />
+        `;
+    document.getElementById('acticle').innerHTML = article;
+    
+        // Check if the user is already logged in
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            var articles = document.getElementById('articles');
+            if (request.status === 200) {
+                var content = '<ul>';
+                var articleData = JSON.parse(this.responseText);
+                for (var i=0; i< articleData.length; i++) {
+                    content += `<li>
+                    <a href="/articles/${title[i].title}">${content[i].heading}</a>
+                    (${author[i].author.split('T')[0]})</li>`;
+                }
+                content += "</ul>";
+                articles.innerHTML = content;
+            } else {
+                articles.innerHTML('Oops! Could not load all articles!');
+            }
+        }
+    };
+    
+    request.open('GET', '/get-articles', true);
+    request.send(null);
+}
+
+
+// The first thing to do is to check if the user is logged in!
+
+// Now this is something that we could have directly done on the server-side using templating too!
+loadArticles();
