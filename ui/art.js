504f44a60fbbfb1ee9c905b38067c50d589f6673
Sathyanarayan09 <sathyanarayan065@gmail.com>
2016-12-01 19:54:38 +0530

    [imad-console] Updates ui/art.js

--- ui/art.js
+++ ui/art.js
@@ -1,4 +1,4 @@
-// Eg: coco98.imad.hasura-app.io/articles/article-one will result in article-one
+
 var currentArticleTitle = window.location.pathname.split('/')[2];
 
 function loadCommentForm () {
@@ -16,15 +16,15 @@ function loadCommentForm () {
     // Submit username/password to login
     var submit = document.getElementById('submit');
     submit.onclick = function () {
-        // Create a request object
+    
         var request = new XMLHttpRequest();
         
-        // Capture the response and store it in a variable
+     
         request.onreadystatechange = function () {
           if (request.readyState === XMLHttpRequest.DONE) {
                 // Take some action
                 if (request.status === 200) {
-                    // clear the form & reload all the comments
+            
                     document.getElementById('comment_text').value = '';
                     loadComments();    
                 } else {
@@ -34,7 +34,7 @@ function loadCommentForm () {
           }
         };
         
-        // Make the request
+       
         var comment = document.getElementById('comment_text').value;
         
               if (comment == '') {
@@ -53,7 +53,7 @@ function loadCommentForm () {
 }
 
 function loadLogin () {
-    // Check if the user is already logged in
+  
     var request = new XMLHttpRequest();
     request.onreadystatechange = function () {
         if (request.readyState === XMLHttpRequest.DONE) {
@@ -76,7 +76,7 @@ function escapeHTML (text)
 }
 
 function loadComments () {
-        // Check if the user is already logged in
+       
     var request = new XMLHttpRequest();
     request.onreadystatechange = function () {
         if (request.readyState === XMLHttpRequest.DONE) {
@@ -105,6 +105,6 @@ function loadComments () {
 }
 
 
-// The first thing to do is to check if the user is logged in!
+
 loadLogin();
 loadComments();
